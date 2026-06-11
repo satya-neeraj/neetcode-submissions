044f7343d42d361c63634345db4a92237b89b36e
satya-neeraj <satyaneerajg@gmail.com>
2026-06-11 15:42:51 +0530

    Add: counting-bits - submission-1

---
+++ Data Structures & Algorithms/counting-bits/submission-1.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    vector<int> countBits(int n) {
+        vector<int>ans(n+1);
+        ans[0]=0;
+        for(int i=1;i<=n;i++)
+        {
+            if(i&1) ans[i]=ans[i-1]+1;
+            else ans[i]=ans[i/2];
+        }
+        return ans;
+    }
+};
